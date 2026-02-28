7bfab5da103c6fe185322c54529a5a31060c65be
Prasanna Rednam <prasannarednam08@gmail.com>
2026-02-28 17:26:43 +0530

    practicisimg shell scripting 2 tym

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON2="ponky" #no space in between variable, = and value
-PERSON1="pinky"
+PERSON1=$1 #no space in between variable, = and value
+PERSON2=$2
 echo "$PERSON1: hello $PERSON2 , how r u?"
 echo "$PERSON2: hello $PERSON1 doing, how r u!!"
 echo "$PERSON1: good good, what are youu learning"
