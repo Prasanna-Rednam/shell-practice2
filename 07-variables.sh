06a194112386146e53180ce44c9f42fcfe9ebd26
Prasanna Rednam <prasannarednam08@gmail.com>
2026-02-28 18:20:40 +0530

    practicisimg shell scripting 2 tym

--- 07-variables.sh
+++ 07-variables.sh
@@ -4,6 +4,7 @@ echo "all arg passed to script: $@ , $*"
 echo "no of arg passed to the script: $#"
 echo "present working directory: $PWD"
 echo "home directory of thr user: $USER"
+sleep 100
 echo "pid of the recent executed background process background process : $!"
 echo "pid of the script-$$"
 echo "script name: $0"
