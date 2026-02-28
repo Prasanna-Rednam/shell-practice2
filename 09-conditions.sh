6ee8c949c04d2f86e889ac93f3991a7e45bc083d
Prasanna Rednam <prasannarednam08@gmail.com>
2026-02-28 18:57:23 +0530

    practicisimg shell scripting 2 tym

---
+++ 09-conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUMBER=$1
+
+if [ $NUMBER -lt 10]; then 
+      echo "less"
+elif [ $NUMBER -gt 10]; then
+      echo "avg"
+elif [ $NUMBER -eq 5]; then
+      echo "num is eq 5"
+elif [ $NUMBER -ne 5]; then
+      echo "num not eq to 5"
+else    
+      echo "not a number"     
+        
