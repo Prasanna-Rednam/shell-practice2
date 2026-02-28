4d33a6f7cee2b07f54fe5340206e86544729d34e
Prasanna Rednam <prasannarednam08@gmail.com>
2026-02-28 18:47:40 +0530

    practicisimg shell scripting 2 tym

--- 08-data-type.sh
+++ 08-data-type.sh
@@ -10,4 +10,8 @@ echo "Sum is: $SUM"
 #array
 FRUITS=("Apple" "maongo" "lichi")
 
+
 echo "fruits are: ${FRUITS[@]}"
+echo "fruits are: ${FRUITS[0]}"
+echo "fruits are: ${FRUITS[1]}"
+echo "fruits are: ${FRUITS[2]}"
