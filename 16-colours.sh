d85742ead5e46b4ba9b26eef2baad06fe198f208
Prasanna Rednam <prasannarednam08@gmail.com>
2026-02-28 23:34:27 +0530

    practicisimg shell scripting 2 tym

--- 16-colours.sh
+++ 16-colours.sh
@@ -13,7 +13,7 @@ LOGS_FILE="/var/log/shell-script/$0.log"
 USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ]; then
-   echo "$RED please run this script with root user access $N"
+   echo -e "$RED please run this script with root user access $N"
    exit 1
 fi
 
@@ -21,10 +21,10 @@ mkdir -p $LOGS_FOLDER
 
 validate(){
     if [ $1 -ne 0 ]; then 
-      echo "$RED $2... failure $N"
+      echo -e "$RED $2... failure $N"
     
    else
-      echo "$GREEN $2 ... sucess $N"
+      echo -e "$GREEN $2 ... sucess $N"
     fi       
 }
 
@@ -33,10 +33,10 @@ for package in $@
 do 
    dnf list installed $package &>>$LOGS_FILE
    if [ $? -ne 0 ]; then
-     echo "$BLUE package is not installed, installing $N $RED $package $N"
+     echo -e "$BLUE package is not installed, installing $N $RED $package $N"
      dnf install $package -y &>>$LOGS_FILE
      validate $? " $BLUE $package installation $N"
     else
-      echo "$YELLOW $package is already installed... skipping $N"
+      echo -e "$YELLOW $package is already installed... skipping $N"
     fi
 done
