e0df16b615a777625c8aef747b2b3221120442f3
Prasanna Rednam <prasannarednam08@gmail.com>
2026-02-28 17:11:29 +0530

    practicisimg shell scripting 2 tym

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "suresh: hello ramesh , how r u?"
+echo "ramesh: hello suresh doing, how r u!!"
+echo "suresh: good good, what are youu learning"
+echo "ramesh: i am learning, shell script"
+echo "suresh: oo okeyyy!!!"
