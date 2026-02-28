f30321584bdb98813867d6e1de1e1b69db6704d1
Prasanna Rednam <prasannarednam08@gmail.com>
2026-02-28 17:34:52 +0530

    practicisimg shell scripting 2 tym

---
+++ 05-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "please enter your user name::"
+read USER_NAME
+
+echo "user name is $USER_NAME"
+
+echo "please enter your password::"
+read -s PASSWORD
+echo "passwd:$PASSWORD"
