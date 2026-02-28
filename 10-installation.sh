b97b7119bbf918143c9f548c3893a954f42d36d3
Prasanna Rednam <prasannarednam08@gmail.com>
2026-02-28 19:37:39 +0530

    practicisimg shell scripting 2 tym

---
+++ 10-installation.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+USER_ID=$(id -u)
+
+if [ $USER_ID -ne 0 ]; then
+   echo "please run this script with root user access"
+   exit 1
+fi
+
+echo "installing Nginx"
+dnf install nginx -y  
