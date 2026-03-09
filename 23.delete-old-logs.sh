f5e34c292d69fb3ec523713bc59a026187af70eb
Prasanna Rednam <prasannarednam08@gmail.com>
2026-03-09 12:48:07 +0530

    script

---
+++ 23.delete-old-logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+RED="\e[31m"
+BLUE="\e[34m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+
+LOGS_DIR="/root/app_logs"
+LOGS_FILE="$LOGS_DIR/$0.log"
+
+if [ ! -d $LOGS_DIR ]; then
+     echo -e "$LOGS_DIR does not exist"
+     exit 1
+fi
+
+FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
+
+echo "$FILES_TO_DELETE"
