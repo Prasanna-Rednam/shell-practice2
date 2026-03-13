9409afc3fae8907dd1074a1b1f7aa87958096633
Prasanna Rednam <prasannarednam08@gmail.com>
2026-03-13 13:17:34 +0530

    script

--- 24.while-loop.sh
+++ 24.while-loop.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
- count=1
+#  count=1
 
- while [ $count -le 5 ]
- do 
-    echo "count is $count"
-    sleep 1
-    #Increment the coounter
-    ((count++))
- done
+#  while [ $count -le 5 ]
+#  do 
+#     echo "count is $count"
+#     sleep 1
+#     #Increment the coounter
+#     ((count++))
+#  done
 
-# #IFS-Internal Feild sepeater 
-# while IFS= read -r LINE; do
-#   # Process the line (e.g., print it)
-#   echo "$LINE"
-# done < script-1.sh # input which file to read 
+#IFS-Internal Feild sepeater 
+while IFS= read -r LINE; do
+  # Process the line (e.g., print it)
+  echo "$LINE"
+done < script-1.sh # input which file to read 
