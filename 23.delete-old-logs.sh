2ae39b077085e502ad3d7a483b5c3db6f629d3c3
Prasanna Rednam <prasannarednam08@gmail.com>
2026-03-13 13:02:56 +0530

    script

--- 23.delete-old-logs.sh
+++ 23.delete-old-logs.sh
@@ -9,17 +9,17 @@ LOGS_DIR="/root/app-logs"
 LOGS_FILE="$LOGS_DIR/$0.log"
 
 if [ ! -d $LOGS_DIR ]; then
-     echo -e "$LOGS_DIR does not exist"
-     exit 1
+      echo -e "$LOGS_DIR does not exist"
+      exit 1
 fi
 
 FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
 
 echo "$FILES_TO_DELETE"
 
- while IFS= read -r filepath; do
- # Process the line (e.g., print it)
-   echo "DELETING FILE: $filepath"
-   rm -f $filepath
-   echo "Deleted file: $filepath"
- done <<< $FILES_TO_DELETE 
+#  while IFS= read -r filepath; do
+#  # Process the line (e.g., print it)
+#    echo "DELETING FILE: $filepath"
+#    rm -f $filepath
+#    echo "Deleted file: $filepath"
+#  done <<< $FILES_TO_DELETE 
